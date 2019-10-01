8a00dbbe1b910e407bb656d9afac63c02258f28f
Eric Harm <ericharm@Erics-MacBook-Pro.local>
2019-09-30 21:59:37 -0400

    sign up sign in sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  devise_scope :user do
-    root to: 'devise/registrations#new'
-  end
+  root to: 'pages#home'
 end
