699e79e589d3458c317736ace1f048211dbeebba
Eric Harm <ericharm@Erics-MacBook-Pro.local>
2019-10-26 13:51:02 -0400

    description sanitizing and uri subbing

--- app/helpers/description_helper.rb
+++ app/helpers/description_helper.rb
@@ -1,9 +1,14 @@
 module DescriptionHelper
+  include ActionView::Helpers::SanitizeHelper
   def description(description)
     if description
-      with_links = description.gsub(/[\s](http[^\s]*)/) { |m| "<a href=\"#{m}\">#{m}</a>" }
-      with_line_breaks = with_links.gsub(/\n\r/, "<br/>")
-      raw(with_line_breaks.gsub(/\n/, "<br/>"))
+      sanitized = strip_tags(description)
+      with_line_breaks = sanitized.gsub(/\n\r/, "<br/>")
+      with_more_line_breaks = with_line_breaks.gsub(/\n/, "<br/>")
+      with_links = with_more_line_breaks.gsub(URI.regexp) do |*m|
+        "<a href=\"#{$&}\">#{$&}</a>" if $&.include?('http')
+      end
+      raw(with_links)
     else
       ''
     end
