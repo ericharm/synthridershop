dfd2ef45ce2fd0fc435cc05461f7e96ac8810aa4
Eric Harm <ericharm@Erics-MacBook-Pro.local>
2019-11-14 13:32:40 -0500

    aux pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # root to: 'pages#home'
   root to:  'bundles#index'
   resources :bundles
-  # resources :users, only: [:show]
   resources :subscriptions, only: [:index, :new]
   resources :payments, only: [:create]
 
   get 'users/:username', to: 'users#show', as: :user
   get 'account', to: 'users#account', as: :account
+
+  get '/about' => 'pages#show', page: 'about', as: :about
+  get '/terms' => 'pages#show', page: 'terms', as: :terms
+  get '/privacy' => 'pages#show', page: 'privacy', as: :privacy
+  get '/legal' => 'pages#show', page: 'legal', as: :legal
+  get '/contribute' => 'pages#show', page: 'contribute', as: :contribute
+  get '/contact' => 'pages#show', page: 'contact', as: :contact
+  get '/help' => 'pages#show', page: 'help', as: :help
 end
